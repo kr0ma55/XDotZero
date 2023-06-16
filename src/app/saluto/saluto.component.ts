cb26bf3ec861c67f04b0ffd8276bb8280bfaba9a
marco.rainone <marco.rainone@eccenture.com>
2023-06-16 13:12:33 +0200

    push per Rober con interpolazione

--- src/app/saluto/saluto.component.ts
+++ src/app/saluto/saluto.component.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-saluto',
   templateUrl: './saluto.component.html',
   styleUrls: ['./saluto.component.scss']
 })
-export class SalutoComponent { }
+export class SalutoComponent implements OnInit{
+
+
+  saluto : Object;
+
+
+
+  ngOnInit(): void {
+  
+    //FACCIAMO FINTA CHE L'OGGETO PROVENGA DA UN BE
+    // E IL SUO VALORE CAMBI OGNI GIORNO
+    this.saluto = {
+      "documentStatus": "ROSSO",
+      "campo2" :"verde"
+    }
+  
+  
+  }
+
+  private cambiaValoreInPagina(): void {
+    if(this.saluto["documentStatus"] === "VERDE"){
+      this.saluto["documentStatus"] = "ROSSO"; 
+      return;
+    }
+    if(this.saluto["documentStatus"] === "ROSSO"){
+      this.saluto["documentStatus"] = "VERDE"; 
+      return;
+    }
+    
+
+  }
+
+
+
+
+
+ }
