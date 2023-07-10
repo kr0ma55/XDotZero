cfd72323b7d762d8e378c8b734624448662555eb
marco.rainone <marco.rainone@eccenture.com>
2023-07-10 12:33:23 +0200

    FEAT : SPIEGAZIONI CLASSI EXTEND E IMPLEMENTS

--- src/app/components/foto-eventi-card/foto-eventi-card.component.ts
+++ src/app/components/foto-eventi-card/foto-eventi-card.component.ts
@@ -1,22 +1,53 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { discardPeriodicTasks } from '@angular/core/testing';
+import { Cane, CiaoMondo, Pianeta, Praticante, Tigre, Universo } from 'src/app/model/praticante.model';
 
 @Component({
   selector: 'app-foto-eventi-card',
   templateUrl: './foto-eventi-card.component.html',
   styleUrls: ['./foto-eventi-card.component.scss']
 })
-export class FotoEventiCardComponent implements OnInit {
+export class FotoEventiCardComponent implements OnInit,OnDestroy {
 
   @Input() titolo;
   @Input() testo;
   @Output() mettiAnsiaAllUtente = new EventEmitter<string>();
-  constructor() { }
+  nuovoCiaMondo : CiaoMondo;
+  nuovoUniverso : Universo;
+  listaPianeti : Pianeta[];
 
-  ngOnInit() {
+  numeroComponente :number  =5678;
+  constructor() { }
+  
+  ngOnDestroy(): void {
+  
+
   }
+
+  ngOnInit(): void {
+    
+    this.nuovoCiaMondo = new CiaoMondo();
+    this.nuovoCiaMondo.saluta();
+    const nuovoPianeta = new Pianeta('Plutone');
+    const nuovoPianetaDue = new Pianeta('Mercurio');
+
+    this.nuovoUniverso = new Universo(nuovoPianeta,nuovoPianetaDue);
+
+    const nuovaTigre = new Tigre();
+    const nuovoCane = new Cane();
+
+  }
+
 
   comunicaAnsia(){
     this.mettiAnsiaAllUtente.emit('ANSIA da parte del figlio');
+  }
+
+
+  clickBottonListaPianeti(){
+
+    this.listaPianeti = this.nuovoUniverso.ottieniPianeti();
+    
   }
 
 }
--- src/app/model/praticante.model.ts
+++ src/app/model/praticante.model.ts
@@ -6,3 +6,74 @@ export interface Praticante {
 export interface PraticanteResponse {
     lista: Praticante[];
 }
+export class CiaoMondo {
+    private salutoPreImpostatoItaliano :string = 'Buongiorno';
+    private salutoPreImpostatoFrancese :string = 'Bonojour';
+    private salutoPreImpostatoInternazionale :string = 'GoodMorning';
+    private lingua : string;
+    constructor(lingua?: string){
+
+        this.lingua = lingua;
+
+     }
+
+    saluta(){
+        
+            (this.lingua === 'ITALIANO') ? console.log(this.salutoPreImpostatoItaliano) : null ;
+            (this.lingua !== 'ITALIANO' && this.lingua !== 'FRANCESE') ? console.log(this.salutoPreImpostatoInternazionale) : null ;
+
+        
+    }      
+}
+
+export class Universo {
+        listaPianeti : Pianeta[];
+        nomeUniverso :string;
+        constructor (paramUno: Pianeta,paramDue: Pianeta,paramTre?: Pianeta){
+            this.listaPianeti.push(paramUno,paramDue);
+        }
+
+        ottieniPianeti(): Pianeta[] {
+            return this.listaPianeti;
+        }
+
+        fornisciNomeUniverso(nomeUniversoComeParametro : string){
+            this.nomeUniverso = nomeUniversoComeParametro
+
+        }
+
+
+}
+
+export class Pianeta {
+
+    private nomePianeta: string ;
+    private nomeGalassia: string ;
+
+    constructor (nomePianetaParam: string,){
+        this.nomePianeta = nomePianetaParam;
+    }
+}
+
+
+export class Mammifero {
+
+   consumaRapporto(){
+
+   }
+}
+export class Tigre extends Mammifero {
+
+    
+    ruggisce(){
+
+    }
+ }
+
+ export class Cane extends Mammifero {
+
+    
+    abbaia(){
+        
+    }
+ }
